c921dff7679d1e069727b36b40a918bbca61ac2b
Elif Balci <mrselifbalci@gmail.com>
2023-11-28 10:41:53 +0000

    Update css import paths

--- client/src/components/starRating.jsx
+++ client/src/components/starRating.jsx
@@ -3,7 +3,7 @@
 
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import "./StarRating.css";
+import "../styles/StarRating.css";
 import { FaStar } from "react-icons/fa";
 
 const StarRating = ({ movieId, rating, onRatingChange }) => {
